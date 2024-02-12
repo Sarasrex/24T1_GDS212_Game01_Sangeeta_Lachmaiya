c2056afd23a2352633371c7b569cbc6f7ac83e2e
SLachmaiyaSAE <1031886@student.sae.edu.au>
2024-02-12 18:56:56 +1100

    Added basic player movement script

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed;
+    public Rigidbody2D rb;
 
-    // Update is called once per frame
+    private Vector2 moveDirection;
+
     void Update()
     {
-        
+        ProcessInputs();
     }
+
+    void FixedUpdate()
+    {
+        Move(); 
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
 }
